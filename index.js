92a8399f50ed1fd2641402579b27c872a9a7b41b
Nwoye Ezekiel <chinecherem.nwoye.cn@gmail.com>
2024-07-23 04:39:52 +0100

    work in progress

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const handleButtonClick = (playAudio) => {
   if (playAudio) document.getElementsByTagName("audio")[0].play();
+
   document
     .getElementsByClassName("music-modal-wrapper")[0]
     .classList.add("fade-out-animation");
 };
 
-document.getElementById("yes-button").addEventListener("click", () => {
-  handleButtonClick(true);
-});
-
 document.getElementById("no-button").addEventListener("click", () => {
   handleButtonClick(false);
+});
+
+document.getElementById("yes-button").addEventListener("click", () => {
+  handleButtonClick(true);
 });
 
 document.addEventListener("DOMContentLoaded", () => {
